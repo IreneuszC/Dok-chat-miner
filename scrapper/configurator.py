0acef3a8e572ef5a83f34e247d79c024f1ecbbec
Krzysztof Hewelt <krzysiu-nv@wp.pl>
2024-03-12 12:41:22 +0100

    Add handlers for DBSCAN and Agglomerate clustering methods. Add more articles

--- scrapper/configurator.py
+++ scrapper/configurator.py
@@ -1,4 +1,4 @@
-PAGES_TO_SCRAP = 2
+PAGES_TO_SCRAP = 5
 DELIMITER = ','  # ',' / ';'
 SCRAPER_DOWNLOADING_DELAY = 2
 SENTENCES_TO_CLEAR = ['An official website of the United States government',
@@ -10,9 +10,18 @@ SENTENCES_TO_CLEAR = ['An official website of the United States government',
                          'CISA also provides a section for',
                          'This updated advisory is a follow-up to the original advisory titled ICSA',
                          'that was published',
-                         'on the ICS webpage on cisa.gov/ICS.',
+                         'on the ICS webpage on',
                          'Successful exploitation of this vulnerability',
-                         'Successful exploitation of these vulnerabilities'
+                         'Successful exploitation of these vulnerabilities',
+                         'we’d welcome your feedback',
+                         'CISA recommends users take defensive measures to minimize the risk of exploitation of these vulnerabilities. CISA reminds organizations to perform proper impact analysis and risk assessment prior to deploying defensive measures.',
+                         'CISA also provides a section for control systems security recommended practices on the ICS webpage on cisa.gov.',
+                         'This product is provided subject to this',
+                         'We recently updated our anonymous',
+                         'We recently updated our anonymous has been assigned to this vulnerability',
+                         'As of January 10 2023 CISA will no longer be updating ICS security advisories for Siemens product vulnerabilities beyond the initial advisory.For the most up-to-date information on vulnerabilities in this advisory please see',
+                         'Several CISA products detailing cyber defense best practices are available for reading and download',
+                         'CISA reminds organizations to perform proper impact analysis and risk assessment prior to deploying defensive measures'
                       ]
 
 CLUSTERS_NUMBER = 3
