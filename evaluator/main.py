e4ee94b8281042f849eaa1159ff7b970f4ac0223
Juliusz Losinski <72278818+juliuszlosinski@users.noreply.github.com>
2024-03-14 22:59:14 +0100

    Reduced summary evaluation

--- evaluator/main.py
+++ evaluator/main.py
@@ -27,29 +27,43 @@ for directory in clustering_results_directories:
         path_to_inside_directory = f"{path_to_directory}/{str(inside_directory)}"
         content_of_inside_directory = os.listdir(path_to_inside_directory)
         print(content_of_inside_directory)
-        data = "article_file, summary_file, score_file, bert/cosine score\n"
+        data = "article_file, summary_file, reduced_summary_file, score_file, bert/cosine score, reduced bert/cosine score\n"
         report_file = open(f"{path_to_inside_directory}/raport.csv", "w+") # Opening the report file (.csv)
         total_score_file = open(f"{path_to_inside_directory}/total_score.txt", "w+") # Openning total bert score file (.txt)
+        total_reduced_score_file = open(f"{path_to_inside_directory}/total_reduced_score.txt", "w+") # Openning total reduced bert score file (.txt)
         total_bert_score:float = 0
+        total_reduced_bert_score:float = 0
         number_of_instances:float = 0
         # Iterating through every file in the directory.
         for file in content_of_inside_directory:
             if not str(file).count("_summary") and not str(file).count("_score") and not str(file).count("csv") and not str(file).count("summary"):
                 print(f"----{file}----")
+                
                 path_to_article_file = f"{path_to_inside_directory}/{file}"
                 path_to_summary_file = path_to_article_file.replace(".txt", "_summary.txt")
                 path_to_score_file = path_to_article_file.replace(".txt", "_score.txt")
+                path_to_reduced_summary_file = path_to_article_file.replace(".txt", "_reduced_summary.txt")
+                path_to_reduced_score_file = path_to_article_file.replace(".txt", "_reduced_score.txt")
+                
+                bert_score = evaluator.evaluate_files(path_to_article_file, path_to_summary_file, path_to_score_file) # Doing the evaluation,
+                reduced_bert_score = evaluator.evaluate_files(path_to_article_file, path_to_reduced_summary_file, path_to_reduced_score_file) # Doing the evaluation,
+                total_bert_score += bert_score
+                total_reduced_bert_score += reduced_bert_score
+                number_of_instances += 1
+                
                 name_of_article_file = file
                 name_of_summary_file = name_of_article_file.replace(".txt", "_summary.txt")
                 name_of_score_file = name_of_article_file.replace(".txt", "_score.txt")
-                bert_score = evaluator.evaluate_files(path_to_article_file, path_to_summary_file, path_to_score_file) # Doing the evaluation,
-                total_bert_score += bert_score
-                number_of_instances += 1
+                name_of_reduced_summary_file = name_of_article_file.replace(".txt", "_reduced_summary.txt")
+                name_of_reduced_score_file = name_of_article_file.replace(".txt", "_reduced_score.txt")
+                
                 print(f"\nPath to article file: {path_to_article_file}")
                 print(f"Path to summary file: {path_to_summary_file}")
+                print(f"Path to reduced summary file: {path_to_reduced_summary_file}")
                 print(f"Path to score file: {path_to_score_file}")
-                print(f"Bert/cosine score: {bert_score}\n")
-                data += f"{name_of_article_file}, {name_of_summary_file}, {name_of_score_file}, {bert_score}\n"
+                print(f"Bert/cosine score: {bert_score}")
+                print(f"Reduced Bert/cosine score: {reduced_bert_score}\n")
+                data += f"{name_of_article_file}, {name_of_summary_file}, {name_of_reduced_summary_file}, {name_of_score_file}, {bert_score}, {reduced_bert_score}\n"
         report_file.write(data)
         report_file.close() # Closing the csv file.
         
@@ -57,3 +71,8 @@ for directory in clustering_results_directories:
         print(f"Total bert/cosine score: {total_bert_score}\n")
         total_score_file.write(str(total_bert_score))
         total_score_file.close() # Closing the total score file.
+        
+        total_reduced_bert_score = total_reduced_bert_score / number_of_instances
+        print(f"Total reduced bert/cosine score: {total_reduced_bert_score}\n")
+        total_reduced_score_file.write(str(total_reduced_bert_score))
+        total_reduced_score_file.close() # Closing the total reduced score file.
